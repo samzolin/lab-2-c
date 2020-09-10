b3ff5a8d42d66767267cdf0a2a22e484896b8944
samzolin <saz5193@psu.edu>
2020-09-10 19:13:51 +0000

    added line

--- main.c
+++ main.c
@@ -2,48 +2,51 @@
 //Collaborator: 
 //Collaborator:
 //Collaborator:
-//Section:
+//Section: 1
 //Break out:
 #include <stdio.h>
 #include <readline/readline.h>
+#include <string.h>
 #include <stdlib.h>
 
 int main(void) {
-  
-  double d;
-  printf("Enter your CMPSC 131 grade: ");
-
-  scanf("%lf", &d);
-
-  if(d>=93.0)
-    printf("Your letter grade for CMPSC is A.");
-  
-  else if(d>=90.0)
-    printf("Your letter grade for CMPSC is A-.");
-   
-  else if(d>=87.0)
-    printf("Your letter grade for CMPSC is B+.");
-  
-  else if(d>=83.0)
-    printf("Your letter grade for CMPSC is B.");
-  
-  else if(d>=80.0)
-    printf("Your letter grade for CMPSC is B-.");
-  
-  else if(d>=77.0)
-    printf("Your letter grade for CMPSC is C+.");
-  
-  else if(d>=70.0)
-    printf("Your letter grade for CMPSC is C.");
-  
-  else if(d>=60.0)
-    printf("Your letter grade for CMPSC is D.");
-  
-  else if(d<60.0)
-    printf("Your letter grade for CMPSC is F.");
-  
-  else
+  char *lettergrade = readline("Enter your CMPSC 131 grade: ");
+  double grade = atof(lettergrade);
+  void printgrade ();
+  if(grade >= 93.0){
+    printf("Your letter grade for CMPSC is A.\n");
+  }
+    else if(grade >= 90.0) {
+    printf("Your letter grade for CMPSC is A-.\n");
+  }
+    else if(grade >= 87.0) {
+    printf("Your letter grade for CMPSC is B+.\n");
+  }
+    else if(grade >= 83.0) {
+    printf("Your letter grade for CMPSC is B.\n");
+  }
+    else if(grade >= 80.0) {
+    printf("Your letter grade for CMPSC is B-.\n");
+  }
+    else if(grade >= 77.0) {
+    printf("Your letter grade for CMPSC is C+.\n");
+  }
+    else if(grade >= 70.0) {
+    printf("Your letter grade for CMPSC is C.\n");
+  }
+    else if(grade >= 60.0) {
+    printf("Your letter grade for CMPSC is D.\n");
+  }
+    else if(grade < 60.0) {
+    printf("Your letter grade for CMPSC is F.\n");
+  }
+    else {
     printf("Invalid");
-
+  }
   return 0;
 }
+
+ 
+
+
+  
