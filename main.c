4f07fc9600a5a616cc698ce5a40e7c4443e660f9
samzolin <saz5193@psu.edu>
2020-09-10 18:34:42 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,32 @@
+//Author: Samantha Zolin saz5193@psu.edu
+//Collaborator: 
+//Collaborator:
+//Collaborator:
+#include <stdio.h>
+
+int main(void) {
+  double d;
+  printf("Enter your CMPSC 131 grade: ");
+  scanf("%lf",&d);
+
+  if(d>=93.0)
+    printf("Your letter grade for CMPSC is A.");
+    else if(d>=90.0)
+    printf("Your letter grade for CMPSC is A-.");
+    else if(d>=87.0)
+    printf("Your letter grade for CMPSC is B+.");
+    else if(d>=83.0)
+    printf("Your letter grade for CMPSC is B.");
+    else if(d>=80.0)
+    printf("Your letter grade for CMPSC is B-.");
+    else if(d>=77.0)
+    printf("Your letter grade for CMPSC is C+.");
+    else if(d>=70.0)
+    printf("Your letter grade for CMPSC is C.");
+    else if(d>=60.0)
+    printf("Your letter grade for CMPSC is D.");
+    else if(d<60.0)
+    printf("Your letter grade for CMPSC is F.");
+
+  return 0;
+}
