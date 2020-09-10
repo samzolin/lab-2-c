eb19454da75f1434510083ad4b5e2f472acd72fc
samzolin <saz5193@psu.edu>
2020-09-10 18:39:50 +0000

    removed line

--- main.c
+++ main.c
@@ -5,29 +5,40 @@
 #include <stdio.h>
 
 int main(void) {
-  double d;
   printf("Enter your CMPSC 131 grade: ");
-  scanf("%lf",&d);
+  double d;
+  
+
+  scanf("%lf", &d);
 
   if(d>=93.0)
     printf("Your letter grade for CMPSC is A.");
-    else if(d>=90.0)
+  
+  else if(d>=90.0)
     printf("Your letter grade for CMPSC is A-.");
-    else if(d>=87.0)
+   
+  else if(d>=87.0)
     printf("Your letter grade for CMPSC is B+.");
-    else if(d>=83.0)
+  
+  else if(d>=83.0)
     printf("Your letter grade for CMPSC is B.");
-    else if(d>=80.0)
+  
+  else if(d>=80.0)
     printf("Your letter grade for CMPSC is B-.");
-    else if(d>=77.0)
+  
+  else if(d>=77.0)
     printf("Your letter grade for CMPSC is C+.");
-    else if(d>=70.0)
+  
+  else if(d>=70.0)
     printf("Your letter grade for CMPSC is C.");
-    else if(d>=60.0)
+  
+  else if(d>=60.0)
     printf("Your letter grade for CMPSC is D.");
-    else if(d<60.0)
+  
+  else if(d<60.0)
     printf("Your letter grade for CMPSC is F.");
-    else
+  
+  else
     printf("Invalid");
 
   return 0;
